1474de40d13f574f8f289dfa575176e94bd284cb
vel <vmv2k05@gmail.com>
2025-01-14 12:55:01 +0530

    modified

--- hospital.py
+++ hospital.py
@@ -23,62 +23,62 @@ class Hospital:
         inFrame = tk.Frame(self.root, bd=4, relief="groove", bg=self.clr(169, 202, 226))
         inFrame.place(width=self.width / 3, height=self.height - 180, x=30, y=100)
 
-        idLbl = tk.Label(inFrame, text="Enter ID:", bg=self.clr(169, 202, 226), font=("Arial", 15, "bold"))
+        idLbl = tk.Label(inFrame, text="Enter ID:", bg=self.clr(169, 202, 226), font=("Times New Roman", 15, "bold"))
         idLbl.grid(row=0, column=0, padx=20, pady=15, sticky="w")
-        self.idIn = tk.Entry(inFrame, width=20, bd=2, font=("Arial", 15))
+        self.idIn = tk.Entry(inFrame, width=20, bd=2, font=("Times New Roman", 15))
         self.idIn.grid(row=0, column=1, padx=10, pady=15)
 
-        nameLbl = tk.Label(inFrame, text="Enter Name:", bg=self.clr(169, 202, 226), font=("Arial", 15, "bold"))
+        nameLbl = tk.Label(inFrame, text="Enter Name:", bg=self.clr(169, 202, 226), font=("Times New Roman", 15, "bold"))
         nameLbl.grid(row=1, column=0, padx=20, pady=15, sticky="w")
-        self.nameIn = tk.Entry(inFrame, width=20, bd=2, font=("Arial", 15))
+        self.nameIn = tk.Entry(inFrame, width=20, bd=2, font=("Times New Roman", 15))
         self.nameIn.grid(row=1, column=1, padx=10, pady=15)
 
-        bgLbl = tk.Label(inFrame, text="Blood Group:", bg=self.clr(169, 202, 226), font=("Arial", 15, "bold"))
+        bgLbl = tk.Label(inFrame, text="Blood Group:", bg=self.clr(169, 202, 226), font=("Times New Roman", 15, "bold"))
         bgLbl.grid(row=2, column=0, padx=20, pady=15, sticky="w")
-        self.bgIn = tk.Entry(inFrame, width=20, bd=2, font=("Arial", 15))
+        self.bgIn = tk.Entry(inFrame, width=20, bd=2, font=("Times New Roman", 15))
         self.bgIn.grid(row=2, column=1, padx=10, pady=15)
 
-        desLbl = tk.Label(inFrame, text="Disease:", bg=self.clr(169, 202, 226), font=("Arial", 15, "bold"))
+        desLbl = tk.Label(inFrame, text="Disease:", bg=self.clr(169, 202, 226), font=("Times New Roman", 15, "bold"))
         desLbl.grid(row=3, column=0, padx=20, pady=15, sticky="w")
-        self.desIn = tk.Entry(inFrame, width=20, bd=2, font=("Arial", 15))
+        self.desIn = tk.Entry(inFrame, width=20, bd=2, font=("Times New Roman", 15))
         self.desIn.grid(row=3, column=1, padx=10, pady=15)
 
-        hpLbl = tk.Label(inFrame, text="Health Points:", bg=self.clr(169, 202, 226), font=("Arial", 15, "bold"))
+        hpLbl = tk.Label(inFrame, text="Health Points:", bg=self.clr(169, 202, 226), font=("Times New Roman", 15, "bold"))
         hpLbl.grid(row=4, column=0, padx=20, pady=15, sticky="w")
-        self.hpIn = tk.Entry(inFrame, width=20, bd=2, font=("Arial", 15))
+        self.hpIn = tk.Entry(inFrame, width=20, bd=2, font=("Times New Roman", 15))
         self.hpIn.grid(row=4, column=1, padx=10, pady=15)
 
-        medLbl = tk.Label(inFrame, text="Medication:", bg=self.clr(169, 202, 226), font=("Arial", 15, "bold"))
+        medLbl = tk.Label(inFrame, text="Medication:", bg=self.clr(169, 202, 226), font=("Times New Roman", 15, "bold"))
         medLbl.grid(row=5, column=0, padx=20, pady=15, sticky="w")
-        self.medIn = tk.Entry(inFrame, width=20, bd=2, font=("Arial", 15))
+        self.medIn = tk.Entry(inFrame, width=20, bd=2, font=("Times New Roman", 15))
         self.medIn.grid(row=5, column=1, padx=10, pady=15)
 
-        addrLbl = tk.Label(inFrame, text="Address:", bg=self.clr(169, 202, 226), font=("Arial", 15, "bold"))
+        addrLbl = tk.Label(inFrame, text="Address:", bg=self.clr(169, 202, 226), font=("Times New Roman", 15, "bold"))
         addrLbl.grid(row=6, column=0, padx=20, pady=15, sticky="w")
-        self.addrIn = tk.Entry(inFrame, width=20, bd=2, font=("Arial", 15))
+        self.addrIn = tk.Entry(inFrame, width=20, bd=2, font=("Times New Roman", 15))
         self.addrIn.grid(row=6, column=1, padx=10, pady=15)
 
-        okBtn = tk.Button(inFrame, text="Admit", command=self.insertFun, bd=2, relief="raised", bg=self.clr(0, 142, 220), font=("Arial", 20, "bold"), width=20)
+        okBtn = tk.Button(inFrame, text="Admit", command=self.insertFun, bd=2, relief="raised", bg=self.clr(0, 142, 220), font=("Times New Roman", 20, "bold"), width=20)
         okBtn.grid(padx=30, pady=25, columnspan=2)
 
         self.detFrame = tk.Frame(self.root, bd=4, relief="groove", bg=self.clr(197, 223, 199))
         self.detFrame.place(width=self.width / 2 + 180, height=self.height - 180, x=self.width / 3 + 60, y=100)
 
-        pIdLbl = tk.Label(self.detFrame, text="Patient ID:", bg=self.clr(197, 223, 199), font=("Arial", 15))
+        pIdLbl = tk.Label(self.detFrame, text="Patient Name:", bg=self.clr(197, 223, 199), font=("Times New Roman", 15))
         pIdLbl.grid(row=0, column=0, padx=10, pady=15, sticky="w")
-        self.pIdIn = tk.Entry(self.detFrame, bd=1, width=12, font=("Arial", 15))
+        self.pIdIn = tk.Entry(self.detFrame, bd=1, width=12, font=("Times New Roman", 15))
         self.pIdIn.grid(row=0, column=1, padx=7, pady=15)
 
-        medicBtn = tk.Button(self.detFrame, command=self.medicsFun, text="Medication", width=10, font=("Arial", 15, "bold"), bd=2, relief="raised")
+        medicBtn = tk.Button(self.detFrame, command=self.medicsFun, text="Medication", width=10, font=("Times New Roman", 15, "bold"), bd=2, relief="raised")
         medicBtn.grid(row=0, column=2, padx=8, pady=15)
 
-        hpBtn = tk.Button(self.detFrame, command=self.hPointFun, text="Health Points", width=10, font=("Arial", 15, "bold"), bd=2, relief="raised")
+        hpBtn = tk.Button(self.detFrame, command=self.hPointFun, text="Health Points", width=10, font=("Times New Roman", 15, "bold"), bd=2, relief="raised")
         hpBtn.grid(row=0, column=3, padx=8, pady=15)
 
-        disBtn = tk.Button(self.detFrame, command=self.disFun, text="Discharge", width=10, font=("Arial", 15, "bold"), bd=2, relief="raised")
+        disBtn = tk.Button(self.detFrame, command=self.disFun, text="Discharge", width=10, font=("Times New Roman", 15, "bold"), bd=2, relief="raised")
         disBtn.grid(row=0, column=4, padx=8, pady=15)
 
-        viewAllBtn = tk.Button(self.detFrame, command=self.viewAllPatients, text="View All Patients", width=15, font=("Arial", 15, "bold"), bd=2, relief="raised", bg=self.clr(0, 142, 220))
+        viewAllBtn = tk.Button(self.detFrame, command=self.viewAllPatients, text="View All Patients", width=15, font=("Times New Roman", 15, "bold"), bd=2, relief="raised", bg=self.clr(0, 142, 220))
         viewAllBtn.grid(row=0, column=5, padx=8, pady=15)
 
         self.tabFun()
@@ -207,20 +207,40 @@ class Hospital:
 
 
     def hPointFun(self):
-        name = self.pIdIn.get().strip()
+        self.pointFrame = tk.Frame(self.root, bg="light gray", bd=3, relief="ridge")
+        self.pointFrame.place(width=400, height=150, x=500, y=200)
+        
+        lbl = tk.Label(self.pointFrame, text="Enter Point:", bg="light gray", font=("Times New Roman", 15, "bold"))
+        lbl.grid(row=0, column=0, padx=20, pady=20)
+        
+        self.pointIn = tk.Entry(self.pointFrame, width=17, bd=2, font=("Times New Roman", 15, "bold"))
+        self.pointIn.grid(row=0, column=1, padx=10, pady=20)
+        
+        
+        okBtn = tk.Button(self.pointFrame, command=self.addPoint, text="Add Point", bd=2, relief="raised", font=("Times New Roman", 15, "bold"), width=15,bg=self.clr(157, 112, 207))
+        okBtn.grid(row=1, column=0, padx=10, pady=10, columnspan=2)
+
+    def addPoint(self):
+        name = self.pIdIn.get().strip()  # Assuming pIdIn now holds the patient's name input
+        hp_input = self.pointIn.get().strip()
+
         if name:
             patient = self.collection.find_one({"name": name})
             if patient:
-                new_points = int(self.hpIn.get()) + patient["hPoint"]
-                self.collection.update_one({"name": name}, {"$set": {"hPoint": new_points}})
-                tk.messagebox.showinfo("Success", f"Updated Health Points for patient {name}.")
-                self.viewAllPatients()
-                self.clearFun()
+                try:
+                    new_points = int(hp_input)
+                    updated_points = new_points + patient["hPoint"]
+                    self.collection.update_one({"name": name}, {"$set": {"hPoint": updated_points}})
+                    tk.messagebox.showinfo("Success", f"Updated Health Points for patient {name}.")
+                    self.viewAllPatients()
+                    self.clearFun()
+                    self.pointFrame.destroy()  # Close the frame after updating
+                except ValueError:
+                    tk.messagebox.showerror("Error", "Please enter a valid number for Health Points.")
             else:
                 tk.messagebox.showerror("Error", "Patient not found.")
         else:
             tk.messagebox.showerror("Error", "Please enter the patient's name.")
-
 
     def disFun(self):
         name = self.pIdIn.get().strip()
